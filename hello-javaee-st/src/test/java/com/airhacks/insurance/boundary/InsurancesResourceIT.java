a5f4ca52eb02489b7728777acff63413fd6c230f
Adam Bien <abien@adam-bien.com>
2016-03-17 10:39:32 +0100

    System test for REST endpoints implemented

---
+++ hello-javaee-st/src/test/java/com/airhacks/insurance/boundary/InsurancesResourceIT.java
@@ -0,0 +1,35 @@
+package com.airhacks.insurance.boundary;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class InsurancesResourceIT {
+
+    private Client client;
+    private WebTarget tut;
+
+    @Before
+    public void init() {
+        this.client = ClientBuilder.newClient();
+        this.tut = this.client.target("http://localhost:8080/hello-javaee/resources/insurances");
+    }
+
+    @Test
+    public void insurances() {
+        Response response = this.tut.request().get();
+        assertThat(response.getStatus(), is(200));
+        String result = response.readEntity(String.class);
+        assertThat(result, is("vehicle"));
+    }
+
+}
