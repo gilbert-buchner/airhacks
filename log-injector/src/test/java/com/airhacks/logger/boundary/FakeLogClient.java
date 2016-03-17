27a796421ea2fb2bc7f127df0bc94f1d79cfaae0
Adam Bien <abien@adam-bien.com>
2016-03-17 14:02:36 +0100

    delta spike injects @Stateless EJB

--- log-injector/src/test/java/com/airhacks/logger/boundary/FakeLogClient.java
+++ log-injector/src/test/java/com/airhacks/logger/boundary/FakeLogClient.java
@@ -1,12 +1,14 @@
 package com.airhacks.logger.boundary;
 
 import java.util.logging.Logger;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 /**
  *
  * @author airhacks.com
  */
+@Stateless
 public class FakeLogClient {
 
     @Inject
