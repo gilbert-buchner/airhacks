e49904e22c9214dff292ba9d7dc2416a52affeb9
Adam Bien <abien@adam-bien.com>
2016-03-17 09:53:28 +0100

    Matchers explained

--- hello-junit/src/test/java/com/airhacks/HelloTest.java
+++ hello-junit/src/test/java/com/airhacks/HelloTest.java
@@ -1,7 +1,14 @@
 package com.airhacks;
 
+import java.util.Arrays;
+import java.util.List;
 import static org.hamcrest.CoreMatchers.both;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
@@ -26,6 +33,16 @@ public class HelloTest {
         assertNotNull(actual);
         assertThat(actual, both(containsString("hello")).
                 and(containsString("world")));
+    }
+
+    @Test
+    public void list() {
+        List<String> words = Arrays.asList("java", "javaee", "june");
+        assertThat(words, hasItem("java"));
+        assertThat(words, both(hasItem("java")).and(hasItem("june")));
+        assertThat(words, hasItems("java", "javaee"));
+        assertThat(words, everyItem(startsWith("j")));
+        assertThat(words, everyItem(not(startsWith(".net"))));
     }
 
 }
